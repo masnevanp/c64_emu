7931fe2254474bccefecbfdf60bb9e9f50c96762
masnevanp <masnevanp@users.noreply.github.com>
2019-07-18 19:31:29 +0300

    Refactor & tweak IEC

--- src/iec.h
+++ src/iec.h
@@ -1,8 +1,11 @@
 #ifndef IEC_H_INCLUDED
 #define IEC_H_INCLUDED
 
-
+#include <vector>
+#include <map>
+#include <algorithm>
 #include "nmos6502/nmos6502.h"
+#include "system.h"
 
 
 namespace IEC {
@@ -46,35 +49,185 @@ public:
 };
 
 
-class Controller : public Device {
+class Volatile_disk : public Device {
 public:
-    Controller() {
-        static Null_device null_dev;
-        for (u8 pa = 0; pa < 0x20; ++pa) {
-            dev[pa] = &null_dev;
+
+    virtual IO_ST talk(u8 sa) {
+        act_ch = &ch[sa & 0xf];
+        if (act_ch->buf == &cmd_buf) {
+            std::string name(cmd_buf.begin(), cmd_buf.end());
+            auto file = storage.find(name);
+            if (file != storage.end()) {
+                act_ch->buf = &(file->second);
+                act_ch->read_pos = 0;
+            } else {
+                act_ch->buf = nullptr;
+            }
         }
-        act = &null_dev;
+
+        return IO_ST::ok;
     }
 
-    virtual IO_ST talk(u8 pa)        { act->sleep(); act = dev[pa & 0x1f]; return IO_ST::ok; }
-    virtual IO_ST listen(u8 pa)      { act->sleep(); act = dev[pa & 0x1f]; return IO_ST::ok; }
+    virtual IO_ST listen(u8 sa) {
+        enum command : u8 { select = 0x6, close = 0xe, open = 0xf, };
 
-    virtual IO_ST read(u8& d)        { return act->read(d);  }
-    virtual IO_ST write(const u8& d) { return act->write(d); }
+        u8 ch_id = sa & 0xf;
+        command cmd = (command)(sa >> 4);
+        switch (cmd) {
+            case command::select:
+                act_ch = &ch[ch_id];
+                if (act_ch->buf == &cmd_buf) {
+                    std::string name(cmd_buf.begin(), cmd_buf.end());
+                    act_ch->buf = &storage[name];
+                    act_ch->read_pos = -1;
+                }
+                break;
+            case command::close:
+                ch[ch_id].buf = nullptr;
+                break;
+            case command::open:
+                cmd_buf.clear();
+                act_ch = &ch[ch_id];
+                act_ch->buf = &cmd_buf;
+                break;
+        }
 
-    IO_ST talk_s(u8 sa)              { return act->talk(sa);   }
-    IO_ST listen_s(u8 sa)            { return act->listen(sa); }
+        return IO_ST::ok;
+    }
+
+    virtual IO_ST read(u8& d) {
+        if (act_ch->buf && act_ch->read_pos > -1) {
+            if (act_ch->read_pos < (int)act_ch->buf->size())
+                d = (*act_ch->buf)[act_ch->read_pos++];
+            if (act_ch->read_pos == (int)act_ch->buf->size()) return IO_ST::eof;
+            else return IO_ST::ok;
+        } else {
+            return IO_ST::timeout_r;
+        }
+    }
+
+    virtual IO_ST write(const u8& d) {
+        if (act_ch->buf) {
+            act_ch->buf->push_back(d);
+            return IO_ST::ok;
+        } else {
+            return IO_ST::timeout_w;
+        }
+    }
+
+private:
+    using buffer = std::vector<u8>;
+
+    struct Ch {
+        buffer* buf = nullptr;
+        int read_pos; // -1 = not for reading
+    };
+
+    Ch ch[16];
+    Ch* act_ch;
+    buffer cmd_buf;
+    std::map<std::string, buffer> storage;
+};
+
+
+class Controller : public Device {
+public:
+    enum { pa_mask = 0x1f, device_count = 0x20 };
+
+    Controller() {
+        static Null_device null_dev;
+        std::fill_n(dev, device_count, &null_dev);
+    }
+
+    virtual IO_ST talk(u8 pa)        { return select(pa); }
+    virtual IO_ST listen(u8 pa)      { return select(pa); }
+
+    virtual IO_ST read(u8& d)        { return dev[act]->read(d);  }
+    virtual IO_ST write(const u8& d) { return dev[act]->write(d); }
+
+    IO_ST talk_s(u8 sa)              { return dev[act]->talk(sa);   }
+    IO_ST listen_s(u8 sa)            { return dev[act]->listen(sa); }
 
     bool attach(Device& d, u8 pa) {
-        if (pa >= 0x1f) return false; // keeping 0x1f as null device
+        if (pa >= (device_count - 1)) return false; // last is always null device
         dev[pa] = &d;
         return true;
     }
 
 private:
-    Device* dev[32];
-    Device* act;
+    IO_ST select(u8 pa) { dev[act]->sleep(); act = pa & pa_mask; return IO_ST::ok; }
+
+    Device* dev[device_count];
+    u8 act = 0x1f;
 };
+
+
+enum IEC_routine : u8 {
+    untlk = 0, talk, unlsn, listen, tksa, second, acptr, ciout
+};
+
+
+void patch_kernal_traps(u8* kernal) {
+    static const u16 IEC_routine_addr[8] = {
+    //  untlk,  talk,   unlsn,  listen, tksa,   second, acptr,  ciout
+        0xedef, 0xed09, 0xedfe, 0xed0c, 0xedc7, 0xedb9, 0xee13, 0xeddd
+    };
+    for (int i = 0; i < 8; ++i) {
+        u16 addr = IEC_routine_addr[i] & 0x1fff;
+        kernal[addr] = (i << 4) | 0x02; // use halting instructions 0x02, 0x12,... ,0x72
+        kernal[addr + 1] = 0x60; // 'rts'
+    }
+}
+
+
+bool handle_trap(System::C64& c64, IEC::Virtual::Controller& iec_ctrl) {
+    enum IEC_command : u8 { listen = 0x20, unlisten = 0x3f, talk = 0x40, untalk = 0x5f };
+
+    // TODO: verify that the pc is what is should be (to dodge faulty traps)?
+
+    IEC::IO_ST status = IEC::IO_ST::ok;
+    u8 iec_routine = c64.cpu.ir >> 4;
+    switch (iec_routine) {
+        case IEC_routine::untlk:
+            c64.cpu.a = IEC_command::untalk;
+            iec_ctrl.talk(c64.cpu.a);
+            break;
+        case IEC_routine::talk:
+            c64.cpu.a |= IEC_command::talk;
+            iec_ctrl.talk(c64.cpu.a);
+            break;
+        case IEC_routine::unlsn:
+            c64.cpu.a = IEC_command::unlisten;
+            iec_ctrl.listen(c64.cpu.a);
+            break;
+        case IEC_routine::listen:
+            c64.cpu.a |= IEC_command::listen;
+            iec_ctrl.listen(c64.cpu.a);
+            break;
+        case IEC_routine::tksa:
+            status = iec_ctrl.talk_s(c64.cpu.a);
+            c64.cpu.clr(NMOS6502::Flag::N);
+            break;
+        case IEC_routine::second:
+            status = iec_ctrl.listen_s(c64.cpu.a);
+            break;
+        case IEC_routine::acptr:
+            status = iec_ctrl.read(c64.cpu.a);
+            break;
+        case IEC_routine::ciout:
+            status = iec_ctrl.write(c64.cpu.a);
+            break;
+        default:
+            return false;
+    }
+
+    c64.cpu.clr(NMOS6502::Flag::C);
+    c64.ram[0x90] |= status; // TODO: should it be cleared in some cases??
+
+    --c64.cpu.pc;  // step back to 'rts'
+
+    return true;
+}
 
 
 } // namespace Virtual
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <map>
 
 #include "nmos6502/nmos6502.h"
 #include "nmos6502/nmos6502_core.h"
@@ -12,38 +10,19 @@
 #include "test.h"
 
 
-enum Trap : u8 {
-    // OPC of the instruction to be used
-    insta_load = 0xf2,
-    iec_base = 0x02, // high nybble will encode the IEC routine id
-};
-
-enum IEC_routine : u8 {
-    untlk = 0, talk, unlsn, listen, tksa, second, acptr, ciout
-};
+static const u8 insta_load_opc = 0xf2;
 
 
 // 'halt' instructions used for trapping
-void patch_kernal_traps(u8* kernal) {
+void patch_insta_load_kernal_trap(u8* kernal) {
     // instant load - trap loading from the tape device (device 1)
-    kernal[0x1539] = Trap::insta_load;
+    kernal[0x1539] = insta_load_opc; // a halting instruction
     kernal[0x153a] = 0xea; // nop
     kernal[0x153b] = 0x60; // rts
-
-    // trap IEC routines
-    static const u16 IEC_routine_addr[8] = {
-    //  untlk,  talk,   unlsn,  listen, tksa,   second, acptr,  ciout
-        0xedef, 0xed09, 0xedfe, 0xed0c, 0xedc7, 0xedb9, 0xee13, 0xeddd
-    };
-    for (int i = 0; i < 8; ++i) {
-        u16 addr = IEC_routine_addr[i] & 0x0fff;
-        kernal[addr] = (i << 4) | Trap::iec_base;
-        kernal[addr + 1] = 0x60; // 'rts'
-    }
 }
 
 
-void do_insta_load(System::C64& c64) {
+void insta_load(System::C64& c64) {
     // IDEAs:
     //   - 'LOAD' or 'LOAD""' loads the directory listing (auto-list somehow?)
     //   - listing has entries for sub/parent directories (parent = '..')
@@ -86,172 +65,21 @@ void do_insta_load(System::C64& c64) {
         c64.cpu.pc = 0xf704; // jmp to 'file not found'
     }
 
-    ++c64.cpu.mcp; // halted --> must bump forward
-
     return;
-}
-
-
-bool do_iec_routine(System::C64& c64, IEC::Virtual::Controller& iec_ctrl) {
-    enum IEC_command : u8 { listen = 0x20, unlisten = 0x3f, talk = 0x40, untalk = 0x5f };
-
-    // TODO: verify that the pc is what is should be (to dodge faulty traps)?
-
-    IEC::IO_ST status = IEC::IO_ST::ok;
-    u8 iec_routine = c64.cpu.ir >> 4;
-    switch (iec_routine) {
-        case IEC_routine::untlk:
-            c64.cpu.a = IEC_command::untalk;
-            iec_ctrl.talk(c64.cpu.a);
-            break;
-        case IEC_routine::talk:
-            c64.cpu.a |= IEC_command::talk;
-            iec_ctrl.talk(c64.cpu.a);
-            break;
-        case IEC_routine::unlsn:
-            c64.cpu.a = IEC_command::unlisten;
-            iec_ctrl.listen(c64.cpu.a);
-            break;
-        case IEC_routine::listen:
-            c64.cpu.a |= IEC_command::listen;
-            iec_ctrl.listen(c64.cpu.a);
-            break;
-        case IEC_routine::tksa:
-            status = iec_ctrl.talk_s(c64.cpu.a);
-            c64.cpu.clr(NMOS6502::Flag::N);
-            break;
-        case IEC_routine::second:
-            status = iec_ctrl.listen_s(c64.cpu.a);
-            break;
-        case IEC_routine::acptr:
-            status = iec_ctrl.read(c64.cpu.a);
-            break;
-        case IEC_routine::ciout:
-            status = iec_ctrl.write(c64.cpu.a);
-            break;
-        default:
-            return false;
-    }
-
-    c64.cpu.clr(NMOS6502::Flag::C);
-    c64.ram[0x90] |= status; // TODO: should it be cleared in some cases??
-
-    ++c64.cpu.mcp; // halted --> bump forward
-    --c64.cpu.pc;  // step back to 'rts'
-
-    return true;
 }
 
 
 class Dummy_device : public IEC::Virtual::Device {
 public:
-    Dummy_device(u8 id_) : id(id_) {}
-
     virtual IEC::IO_ST talk(u8 a)         { msg("t", a); x = a; c = 0; return IEC::IO_ST::ok; }
     virtual IEC::IO_ST listen(u8 a)       { msg("l", a); x = a; c = 0; return IEC::IO_ST::ok; }
     virtual void sleep()                  { msg("s", c); }
     virtual IEC::IO_ST read(u8& d)        { msg("r", x); d = x; ++c; return IEC::IO_ST::eof;}
     virtual IEC::IO_ST write(const u8& d) { msg("w", d); x = d; ++c; return IEC::IO_ST::ok; }
-
-    u8 id;
 private:
     u8 x = 0x00;
     int c = 0;
-    void msg(const char* m, int d) {
-        std::cout << " D" << (int)id << ":" << m << ',' << (int)d;
-    }
-};
-
-
-//static const std::string fn = "data/prg/giana.prg";
-//static auto bin = read_bin_file(fn);
-//static auto sz = bin.size();
-//static auto pos = sz;
-class RAM_disk : public IEC::Virtual::Device {
-public:
-    using buffer = std::vector<u8>;
-
-    enum command : u8 { select = 0x6, close = 0xe, open = 0xf, };
-
-    struct Ch {
-        buffer* buf = nullptr;
-        int read_pos = -1; // -1 = not for reading
-    };
-
-    RAM_disk() {
-        ch[16].buf = &cmd_buf;
-    }
-
-    virtual IEC::IO_ST talk(u8 sa) {
-        if (open_ch_req != 0xff) {
-            std::string name(cmd_buf.begin(), cmd_buf.end());
-            auto file = storage.find(name);
-            if (file != storage.end()) {
-                ch[open_ch_req].buf = &(file->second);
-                ch[open_ch_req].read_pos = 0;
-            }
-            open_ch_req = 0xff; // 'ack'
-        }
-
-        act_ch = &ch[sa & 0xf];
-
-        return IEC::IO_ST::ok;
-    }
-
-    virtual IEC::IO_ST listen(u8 sa) {
-        u8 ch_id = sa & 0xf;
-        command cmd = (command)(sa >> 4);
-        switch (cmd) {
-            case command::select:
-                if (open_ch_req != 0xff) {
-                    std::string name(cmd_buf.begin(), cmd_buf.end());
-                    ch[open_ch_req].buf = &storage[name];
-                    ch[open_ch_req].read_pos = -1;
-                    open_ch_req = 0xff;
-                }
-                act_ch = &ch[ch_id];
-                break;
-            case command::close:
-                ch[ch_id].buf = nullptr;
-                break;
-            case command::open:
-                open_ch_req = ch_id;
-                cmd_buf.clear();
-                act_ch = &ch[16];
-                break;
-        }
-
-        return IEC::IO_ST::ok;
-    }
-
-    // virtual void sleep() { }
-
-    virtual IEC::IO_ST read(u8& d) {
-        if (act_ch->buf) { // TODO: check 'read_pos >= 0' ?
-            if (act_ch->read_pos < (int)act_ch->buf->size())
-                d = (*act_ch->buf)[act_ch->read_pos++];
-            if (act_ch->read_pos == (int)act_ch->buf->size()) return IEC::IO_ST::eof;
-            else return IEC::IO_ST::ok;
-        } else {
-            return IEC::IO_ST::timeout_r; // ok? eof?
-        }
-    }
-
-    virtual IEC::IO_ST write(const u8& d) {
-        if (act_ch->buf) {
-            act_ch->buf->push_back(d);
-            return IEC::IO_ST::ok;
-        } else {
-            return IEC::IO_ST::timeout_w; // ok?
-        }
-    }
-
-private:
-    u8 open_ch_req = 0xff;
-    Ch ch[17];
-    Ch* act_ch;
-    buffer cmd_buf;
-    std::map<std::string, buffer> storage;
+    void msg(const char* m, int d) { std::cout << " DD:" << m << ',' << d; }
 };
 
 
@@ -260,31 +88,33 @@ void run_c64() {
     u8 kernal[0x2000];
     u8 charr[0x1000];
 
-    const std::string rom_dir = "data/c64_roms/";
-    read_bin_file(rom_dir + "basic.rom", (char*)basic);
-    read_bin_file(rom_dir + "kernal.rom", (char*)kernal);
-    read_bin_file(rom_dir + "char.rom", (char*)charr);
+    read_bin_file("data/c64_roms/basic.rom", (char*)basic);
+    read_bin_file("data/c64_roms/kernal.rom", (char*)kernal);
+    read_bin_file("data/c64_roms/char.rom", (char*)charr);
 
-    patch_kernal_traps(kernal);
+    patch_insta_load_kernal_trap(kernal);
+    IEC::Virtual::patch_kernal_traps(kernal);
 
     System::ROM roms{basic, kernal, charr};
     System::C64 c64(roms);
 
     IEC::Virtual::Controller iec_ctrl;
-    Dummy_device dd_a(20);
-    Dummy_device dd_b(30);
-    RAM_disk ram_disk;
-    iec_ctrl.attach(dd_a, dd_a.id);
-    iec_ctrl.attach(dd_b, dd_b.id);
-    iec_ctrl.attach(ram_disk, 10);
+    IEC::Virtual::Volatile_disk vol_disk;
+    Dummy_device dd;
+    iec_ctrl.attach(vol_disk, 10);
+    iec_ctrl.attach(dd, 30);
 
     NMOS6502::Sig on_halt = [&]() {
         // TODO: verify that it is a kernal trap, e.g. 'banker.mapping(cpu.pc) == kernal' ?
-        if (c64.cpu.ir == insta_load) do_insta_load(c64);
-        else if (!do_iec_routine(c64, iec_ctrl)) {
+        if (c64.cpu.ir == insta_load_opc) {
+            insta_load(c64);
+        } else if (!IEC::Virtual::handle_trap(c64, iec_ctrl)) {
             std::cout << "\n****** CPU halted! ******\n";
             Dbg::print_status(c64.cpu, c64.ram);
+            return;
         }
+
+        ++c64.cpu.mcp; // halted --> bump forward
     };
 
     c64.cpu.sig_halt = on_halt;
