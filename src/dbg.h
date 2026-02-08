80b4ef80e879eabeb3f17b9674427aa3ca7c538a
masnevanp <masnevanp@users.noreply.github.com>
2026-02-08 10:57:11 +0700

    dbg: disasm

--- src/dbg.h
+++ src/dbg.h
@@ -25,21 +25,106 @@ void print_status(const Core& cpu, u8* mem);
 void reg_diff(const Core& cpu);
 
 
-struct Instruction {
-    enum class Addr_mode : u8 {
-        impl, abs, absx, absy, imm, ind, indx, indy, rel, zp, zpx, zpy
+template<typename Bin>
+class Disassembler {
+public:
+    struct Line {
+        // TODO: add opc, pos (if needed)
+        const std::string pc; // TODO: change to u16 (if needed)
+        const std::string bytes;
+        const std::string text;
     };
 
-    const u8 code;
-    const u8 size;
-    const Addr_mode addr_mode;
-    const std::string mnemonic;
-    const std::string format;
+    Disassembler(const Bin& bin_, const u16 start_addr_ = 0x0000)
+        : bin(bin_), start_addr(start_addr_) {}
+
+    Line at(const std::size_t bin_pos) const {
+        if (bin_pos >= std::size(bin)) return {"????", "??", "<out of range>"};
+        else return disasm(bin_pos);
+    }
+
+    Line at_pc(const u16 pc) const {
+        if (pc < start_addr) return {"????", "??", "<out of range>"};
+        else return at(pc - start_addr);
+    }
+
+    const Bin& bin;
+    const u16 start_addr;
+
+private:
+    Line disasm(const std::size_t bin_pos) const {
+        const auto opc = bin[bin_pos];
+        const auto& instr = NMOS6502::instruction[opc];
+        const u16 pc = start_addr + bin_pos;
+
+        char instr_disasm[16];
+        char instr_bytes[9]; // max: 3 bytes + 2 spaces + terminator = 6 + 2 + 1 
+
+        const std::size_t byte_cnt = std::size(bin);
+
+        if (bool bytes_missing = (bin_pos + instr.size) > byte_cnt; bytes_missing) {
+            sprintf(instr_disasm, "%s <missing>", instr.mnemonic.c_str());
+
+            const auto extra_bytes = byte_cnt - bin_pos;
+            if (extra_bytes == 1) {
+                sprintf(instr_bytes, "%02X", bin[bin_pos]);
+            } else { // only 1 or 2 possible
+                sprintf(instr_bytes, "%02X %02X", bin[bin_pos], bin[bin_pos + 1]);
+            }
+        } else {
+            switch (instr.size) {
+                case 1:
+                    sprintf(instr_disasm, instr.asm_format.c_str());
+                    sprintf(instr_bytes, "%02X", bin[bin_pos]);
+                    break; 
+                case 2:
+                    if (instr.addr_mode == NMOS6502::Addr_mode::rel) {
+                        const u16 tgt_addr = pc + 2 + i8(bin[bin_pos + 1]);
+                        sprintf(instr_disasm, instr.asm_format.c_str(), tgt_addr);
+                        sprintf(instr_bytes, "%02X %02X", bin[bin_pos], bin[bin_pos + 1]);
+                    } else {
+                        sprintf(instr_disasm, instr.asm_format.c_str(), bin[bin_pos + 1]);
+                        sprintf(instr_bytes, "%02X %02X", bin[bin_pos], bin[bin_pos + 1]);
+                    }
+                    break;
+                case 3:
+                    sprintf(instr_disasm, instr.asm_format.c_str(), bin[bin_pos + 2], bin[bin_pos + 1]);
+                    sprintf(instr_bytes, "%02X %02X %02X", bin[bin_pos], bin[bin_pos + 1], bin[bin_pos + 2]);
+                    break;
+            }
+        }
+
+        return Line {
+            print_u16(pc),
+            std::string(instr_bytes),
+            std::string(instr_disasm)
+        };
+    }
 };
 
-extern const Instruction instruction[256];
+
+template<typename Bin>
+auto disasm(const Bin& bin, const u16 start_addr = 0x0000) {
+    Disassembler dis{bin, start_addr};
+
+    std::vector<typename Disassembler<Bin>::Line> lines;
+
+    for (std::size_t bin_pos = 0; bin_pos < std::size(bin); ) {
+        lines.push_back(dis.at(bin_pos));
+        bin_pos += NMOS6502::instruction[bin[bin_pos]].size;
+    }
+
+    return lines;
+}
 
 
+template<typename Bin>
+auto disasm_first(const Bin& bin, const u16 start_addr = 0x0000) {
+    return Disassembler{bin, start_addr}.at(0);
+}
+
+
+/*
 template<typename Bytes>
 std::vector<std::string> disasm(const Bytes& bytes, const u16 start_addr = 0x0000) {
     std::vector<std::string> asm_list;
@@ -55,7 +140,7 @@ std::vector<std::string> disasm(const Bytes& bytes, const u16 start_addr = 0x000
         char instr_bytes[9]; // max: 3 bytes + 2 spaces + terminator = 6 + 2 + 1 
 
         if (bool bytes_missing = (byte_pos + instr.size) > byte_cnt; bytes_missing) {
-            sprintf(instr_disasm, "%s ???", instr.mnemonic.c_str());
+            sprintf(instr_disasm, "%s <missing>", instr.mnemonic.c_str());
 
             const auto extra_bytes = byte_cnt - byte_pos;
             if (extra_bytes == 1) {
@@ -66,15 +151,21 @@ std::vector<std::string> disasm(const Bytes& bytes, const u16 start_addr = 0x000
         } else {
             switch (instr.size) {
                 case 1:
-                    sprintf(instr_disasm, instr.format.c_str());
+                    sprintf(instr_disasm, instr.asm_format.c_str());
                     sprintf(instr_bytes, "%02X", bytes[byte_pos]);
                     break; 
                 case 2:
-                    sprintf(instr_disasm, instr.format.c_str(), bytes[byte_pos + 1]);
-                    sprintf(instr_bytes, "%02X %02X", bytes[byte_pos], bytes[byte_pos + 1]);
+                    if (instr.addr_mode == NMOS6502::Addr_mode::rel) {
+                        const u16 tgt_addr = pc + 2 + i8(bytes[byte_pos + 1]);
+                        sprintf(instr_disasm, instr.asm_format.c_str(), tgt_addr);
+                        sprintf(instr_bytes, "%02X %02X", bytes[byte_pos], bytes[byte_pos + 1]);
+                    } else {
+                        sprintf(instr_disasm, instr.asm_format.c_str(), bytes[byte_pos + 1]);
+                        sprintf(instr_bytes, "%02X %02X", bytes[byte_pos], bytes[byte_pos + 1]);
+                    }
                     break;
                 case 3:
-                    sprintf(instr_disasm, instr.format.c_str(), bytes[byte_pos + 2], bytes[byte_pos + 1]);
+                    sprintf(instr_disasm, instr.asm_format.c_str(), bytes[byte_pos + 2], bytes[byte_pos + 1]);
                     sprintf(instr_bytes, "%02X %02X %02X", bytes[byte_pos], bytes[byte_pos + 1], bytes[byte_pos + 2]);
                     break;
             }
@@ -89,6 +180,7 @@ std::vector<std::string> disasm(const Bytes& bytes, const u16 start_addr = 0x000
 
     return asm_list;
 }
+*/
 
 
 class System;
--- src/nmos6502/nmos6502.h
+++ src/nmos6502/nmos6502.h
@@ -8,43 +8,58 @@
 
 namespace NMOS6502 {
 
-    using u8 = uint8_t;
-    using i8 = int8_t;
-    using u16 = uint16_t;
+using u8 = uint8_t;
+using i8 = int8_t;
+using u16 = uint16_t;
 
-    using Reg16 = u16;
-    using Reg8 = u8;
+using Reg16 = u16;
+using Reg8 = u8;
 
-    using Sig  = std::function<void (void)>;
+using Sig  = std::function<void (void)>;
 
-    struct Vec { enum { nmi = 0xfffa, rst = 0xfffc, irq = 0xfffe, }; };
+struct Vec { enum { nmi = 0xfffa, rst = 0xfffc, irq = 0xfffe, }; };
 
-    enum Flag : u8 { // nvubdizc, u = unused (bit 5)
-        N = 0b10000000, V = 0b01000000, u = 0b00100000, B = 0b00010000,
-        D = 0b00001000, I = 0b00000100, Z = 0b00000010, C = 0b00000001,
-        all = 0b11111111,
-    };
+enum Flag : u8 { // nvubdizc, u = unused (bit 5)
+    N = 0b10000000, V = 0b01000000, u = 0b00100000, B = 0b00010000,
+    D = 0b00001000, I = 0b00000100, Z = 0b00000010, C = 0b00000001,
+    all = 0b11111111,
+};
 
-    // 16bit reg indices
-    enum Ri16 : u8 {
-        zp16 = 0, pc = 1, d_ir = 2, a1 = 3, a2 = 4, sp16 = 5, p_a = 6, x_y = 7,
-        a3 = 8, a4 = 9,
-        _cnt16 = a4 + 1
-    };
-    extern const std::string Ri16_str[];
+// 16bit reg indices
+enum Ri16 : u8 {
+    zp16 = 0, pc = 1, d_ir = 2, a1 = 3, a2 = 4, sp16 = 5, p_a = 6, x_y = 7,
+    a3 = 8, a4 = 9,
+    _cnt16 = a4 + 1
+};
+extern const std::string Ri16_str[];
 
-    // 8bit reg indices
-    enum Ri8 : u8 { // TODO: big-endian host
-        zp = 0, zph = 1, pcl = 2, pch = 3, d = 4, ir = 5, a1l = 6, a1h = 7, a2l = 8, a2h = 9,
-        sp = 10, sph = 11, p = 12, a = 13, x = 14, y = 15, a3l = 16, a3h = 17, a4l = 18, a4h = 19,
-        _cnt8 = a4h + 1
-    };
-    extern const std::string Ri8_str[];
+// 8bit reg indices
+enum Ri8 : u8 { // TODO: big-endian host
+    zp = 0, zph = 1, pcl = 2, pch = 3, d = 4, ir = 5, a1l = 6, a1h = 7, a2l = 8, a2h = 9,
+    sp = 10, sph = 11, p = 12, a = 13, x = 14, y = 15, a3l = 16, a3h = 17, a4l = 18, a4h = 19,
+    _cnt8 = a4h + 1
+};
+extern const std::string Ri8_str[];
 
-    enum OPC {
-        brk = 0x00, rts = 0x60, bne = 0xd0,
-        reset = 0x100
-    };
+enum OPC {
+    brk = 0x00, rts = 0x60, bne = 0xd0,
+    reset = 0x100
+};
+
+enum class Addr_mode {
+    impl, accu, abs, absx, absy, imm, ind, indx, indy, rel, zp, zpx, zpy
+};
+
+// TODO: (remove '.asm_format' --> e.g. Disassembler.disasm() should switch on addr.mode instead)
+struct Instruction {
+    const u8 code;
+    const u8 size;
+    const NMOS6502::Addr_mode addr_mode;
+    const std::string mnemonic;
+    const std::string asm_format;
+};
+
+extern const Instruction instruction[256]; // TODO: move to 'nmos6502/'
 
 } // namespace NMOS6502
 
